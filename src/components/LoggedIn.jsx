2d84b5973ac17f2deffa18a231634332fae8f909
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-10 14:11:47 +0330

    edit logged in page

--- src/components/LoggedIn.jsx
+++ src/components/LoggedIn.jsx
@@ -4,7 +4,8 @@ function LoggedIn({ setLoggin, inputs }) {
   return (
     <div className="loggedin">
       <h1>Welcome</h1>
-      <h4>Your Username : {inputs.username}</h4>
+      <h4>Your Username : </h4>
+      <h4>{inputs.username}</h4>
       <button className="submit-btn" onClick={() => setLoggin(0)}>Logout</button>
     </div>
   );
